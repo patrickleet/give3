b3c76ec8cc1bcefbf335e0b9cf29f8b3515abf0d
Patrick Scott <patrick.lee.scott@gmail.com>
2014-02-28 11:43:19 -0500

    added stuff

---
+++ router/router.js
@@ -0,0 +1,93 @@
+/* ---------------------------------------------------- +/
+
+## Client Router ##
+
+Client-side Router.
+
+/+ ---------------------------------------------------- */
+
+// Config
+
+Router.configure({
+  layoutTemplate: 'layout',
+  loadingTemplate: 'loading',
+  notFoundTemplate: 'notFound'
+});
+
+// Filters
+
+var filters = {
+
+  isLoggedIn: function() {
+    if (! Meteor.user()) {
+      if (Meteor.loggingIn()) {
+        this.render(this.loadingTemplate);
+      }
+      else {
+        this.render('entrySignIn');
+      }
+      this.stop();
+    }
+  },
+  isLoggedOut: function() {
+    if (Meteor.user()) {
+      this.stop();
+      Router.go('dashboard');
+    }
+  },
+  isAdmin: function() {
+    if (!Roles.userIsInRole(Meteor.userId(), ['admin'])) {
+      this.render('notFound');
+      this.stop();
+    }
+  }
+};
+
+var helpers = {
+  analyticsRequest: function() {
+    // console.log('Make analytics request here', this, Meteor.user());
+  },
+  showLoadingBar: function() {
+    if (this.ready()) {
+      NProgress.done();
+    } else {
+      NProgress.start();
+      this.stop();
+    }
+  }
+};
+
+// If logged in, redirect requests to account pages to dashboard
+Router.before(filters.isLoggedOut, {only: [
+  "entrySignIn",
+  "entrySignUp",
+  "homepage"
+]});
+
+// Check authenticated
+Router.before(filters.isLoggedIn, {only: [
+  'dashboard'
+]});
+
+// Show loading bar for any route that loads a subscription
+Router.before(helpers.showLoadingBar, {only: []});
+
+Router.after(helpers.analyticsRequest);
+
+// Routes
+
+Router.map(function() {
+
+  // Pages
+
+  this.route('homepage', {
+    path: '/'
+  });
+
+  // Dashboard
+
+  this.route('dashboard', {
+    path: '/dashboard'
+  });
+
+});
---
+++ server/inspector.js
@@ -0,0 +1,3 @@
+Meteor.startup(function () {
+  Inspector.runIfDebugging();
+});
