1f18ba491c02e894c869b08596ed817d2d4c4df8
Patrick Scott <patrick.lee.scott@gmail.com>
2014-02-28 14:42:11 -0500

    remove container from layout - each view should decide if it needs a container

--- client/main.js
+++ client/main.js
@@ -11,6 +11,7 @@ Global client-side code. Loads last.
 Meteor.startup(function () {
   FastClick.attach(document.body);
   document.body.addEventListener('touchmove', function(event) {
+    console.log(event.currentTarget);
     event.preventDefault();
   }, false);
 });
