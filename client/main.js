e1563d0516729f134db3a0827c9c512ef0cddd7f
Patrick Scott <patrick.lee.scott@gmail.com>
2014-03-03 10:53:35 -0500

    fixed button

--- client/main.js
+++ client/main.js
@@ -11,7 +11,8 @@ Global client-side code. Loads last.
 Meteor.startup(function () {
   FastClick.attach(document.body);
   document.body.addEventListener('touchmove', function(event) {
-    console.log(event.currentTarget);
-    event.preventDefault();
+    if (!$(event.target).closest('.container')) {
+      event.preventDefault();
+    }
   }, false);
 });
